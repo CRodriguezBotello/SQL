9535a10a1afe67c2b891bc4dc59af7ad8c883fe8
carlos <email_crbotello08@iescastelar.com>
2024-10-24 19:32:41 +0200

    borrar

--- borrar.php
+++ borrar.php
@@ -1,5 +1,12 @@
 <?php
     require_once 'conexion.php';
     $conexion= new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    $conexion->set_charset("utf8");
 
-    $sql= 'DELETE FROM alumnos WHERE ';
+    $sql= 'DELETE FROM alumnos WHERE idAlumno = '.$_POST["idAlumno"];
+    echo $sql;
+    // $resultado=$conexion->query($sql);
+
+    if ($conexion->query($sql)) {
+        echo 'Fila eliminada';
+    }
--- html_borrar.php
+++ html_borrar.php
@@ -1,24 +1,19 @@
-<?php
-    require_once 'mostrar_datos_V2.php'
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="formulario.css">
+    <title>Numero Par o Impar</title>
 </head>
 <body>
-    <table>
-        <tr>
-            <th colspan="2">Tabla de Alumnos</th>
-        </tr>
-        <?php
-             $numfilas=count($fila);
-             for($i=0; $i<=$numfilas; $i++){
-                echo '<tr><td>'.$fila[$i].'</td></tr>';
-             }
-        ?>
-    </table>
+    <form method="POST" action="borrar.php">
+        <fieldset>
+            <caption>Dni a borrar</caption>
+            <br/><br/>
+            <input type="text" name="idAlumno"><br/><br/>
+            <input type="submit" value="Enviar">
+        </fieldset>
+    </form>
 </body>
 </html>
